a7351a5cacf29303da83fa663ec260e64c8ca747
unknown <log2aayusharyan@gmail.com>
2020-09-05 12:37:30 -0700

    computing arithmetic operation

--- ArithmaticComputationAndSorting.sh
+++ ArithmaticComputationAndSorting.sh
@@ -4,3 +4,6 @@ echo "---- Welcome to Arithmetic Compution And Sorting ----"
 read -p "Enter first input :" firstInput
 read -p "Enter second input:" secondInput
 read -p "Enter third input :" thirdInput
+
+result=`echo "scale=2;$firstInput + $secondInput * $thirdInput" | bc`
+echo "Result:" $result
